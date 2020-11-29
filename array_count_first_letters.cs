0e6472552e82120f61f4b80402d5ad4ee6f602d3
karklar1 <arturs.karklins@posti.com>
2020-11-29 14:26:24 +0200

    Refactoring

--- array_count_first_letters.cs
+++ array_count_first_letters.cs
@@ -10,19 +10,25 @@ namespace MultiplyTable
     {
         static void Main(string[] args)
         {
-            string[] fruits = new string[] { "apple", "banana", "mango", "melon", "blackberries", "blueberries"};
+            // input array with strings
+            string[] fruits = new string[] {"apple", "banana", "mango", "melon", "blackberries", "blueberries"};
             
+            // placeholder arrays for storing letters and count
             string [] firstLetters = new string [fruits.Length];
             int [] counts = new int [fruits.Length];
+            
+            // intermediator variables
             string firstLetter;
             int uniqueLetters = 0;
 
+            // loop through array
             for (int i = 0; i < fruits.Length; i++)
             {
                 firstLetter = fruits[i].Substring(0, 1);
                 
+                // check if we have encountered the letter before, if so ++
                 bool found = false;
-                for (int j = 0; j < fruits.Length; j++) {
+                for (int j = 0; j < uniqueLetters; j++) {
                     if (firstLetters[j] == firstLetter) {
                         found = true;
                         counts[j]++;
@@ -30,6 +36,7 @@ namespace MultiplyTable
                     }
                 }
                 
+                // if letter is not found in letter array, add new index and set count to 1
                 if (!found) {
                     firstLetters[uniqueLetters] = firstLetter;
                     counts[uniqueLetters] = 1;
@@ -37,10 +44,9 @@ namespace MultiplyTable
                 }                
             }
             
-            for (int i = 0; i < firstLetters.Length; i++){
-                if (counts[i] != 0)
-                    Console.WriteLine($"{firstLetters[i]}: {counts[i]}");
-            }
+            // print array of letters
+            for (int i = 0; i < uniqueLetters; i++)
+                Console.WriteLine($"{firstLetters[i]}: {counts[i]}");
         }
     }
 }
